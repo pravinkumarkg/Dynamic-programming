682f68f471bef553862feb7da00f981a8f8299d9
pravinkumarkg <35860045+pravinkumarkg@users.noreply.github.com>
2019-07-08 15:43:46 +0530

    Create substring.cpp

---
+++ substring.cpp
@@ -0,0 +1,26 @@
+#include<iostream> 
+#include<string.h>
+using namespace std; 
+void subString(char str[], int n) 
+{ 
+ 
+	for (int len = 1; len <= n; len++) 
+	{	 
+	
+		for (int i = 0; i <= n - len; i++) 
+		{ 
+			
+			int j = i + len - 1;			 
+			for (int k = i; k <= j; k++) 
+				cout << str[k]; 
+			
+			cout << endl; 
+		} 
+	} 
+} 
+int main() 
+{ 
+	char str[] = "parker"; 
+	subString(str, strlen(str)); 
+	return 0; 
+} 
